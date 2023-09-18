95472f8275fd1826098f08da36c83ad6c7528648
ZaraPvtBot <142078798+AlonePvtGithub@users.noreply.github.com>
2023-09-18 13:29:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,6 +21,9 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
+# A name for your Music bot.
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Alone")
+
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6079943111))
 
