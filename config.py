2fc225be4b03a2eefa0f3b43e5057b7a7c56d8b6
ZaraPvtBot <142078798+AlonePvtGithub@users.noreply.github.com>
2023-09-18 21:43:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamAloneOp/AloneXMusic",
+    "https://github.com/AlonePvtGithub/V2Test",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
